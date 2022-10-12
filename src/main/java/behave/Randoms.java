278b54917fb95872c486e8bd6b59928a6089606e
teryokhinfit <terekhincw@gmail.com>
2022-10-13 01:58:31 +0400

    Initial commit

---
+++ src/main/java/behave/Randoms.java
@@ -0,0 +1,31 @@
+package behave;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random = new Random();
+    private int min, max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(min, max +1);
+            }
+        };
+    }
+
+}
